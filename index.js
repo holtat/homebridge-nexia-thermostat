27ecc9840e9a1a396e592e1901cb8a4352e1b7ed
J. Nick Koston <nick@cpanel.net>
2017-11-28 04:03:33 -0600

    less debug

--- index.js
+++ index.js
@@ -174,7 +174,6 @@ NexiaThermostat.prototype = {
   _findTStatInNexiaResponse: function() {
       var data = this._currentData;
 
-      this.log(data);
       var all_items = data.result._links.child[0].data.items;
       var want_tStatId = this.thermostatIndex;
       var tStatId = -1;
